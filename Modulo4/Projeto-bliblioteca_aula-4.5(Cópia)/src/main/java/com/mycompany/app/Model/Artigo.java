40ee2fb3911b00601a74cd9c86a3e53b5a82318c
Henrique Gontijo <henrique.gontijo@pdbomdespacho.com.br>
2024-12-19 18:14:31 -0300

    Add files via upload

---
+++ Modulo4/Projeto-bliblioteca_aula-4.5(Cópia)/src/main/java/com/mycompany/app/Model/Artigo.java
@@ -0,0 +1,25 @@
+package com.mycompany.app.Model;
+
+public class Artigo extends Publicacao {
+    private String genero;
+    private boolean publicado;
+
+    public Artigo(String titulo, Autor autor, String genero, boolean publicado) {
+        super(titulo, autor);
+        this.genero = genero;
+        this.publicado = publicado;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public boolean getPublicado() {
+        return publicado;
+    }
+
+    @Override
+    public void validarPublicacao() {
+        System.out.println("Validando publicação do artigo...");
+    }
+}
